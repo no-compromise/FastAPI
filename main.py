f10d963c2279f141558a15b92496a23eb8aff5de
no-compromise <4andrej@gmail.com>
2022-08-07 15:23:28 +0200

    - Working DB, fetch post from DB, post new in DB

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from turtle import pos
+from fastapi import FastAPI, status
 import psycopg
+from pydantic import BaseModel
+from psycopg.rows import dict_row
 
 try:
     conn = psycopg.connect(
-        host="localhost", dbname="fastapi", user="postgres", password="kokotko123"
+        host="localhost",
+        dbname="rapidapi",
+        user="postgres",
+        password="",
+        row_factory=dict_row,
     )
-    cursor = conn.cursor()
+    cursor = conn.cursor(row_factory=dict_row)
     print("Connection to DB OK!")
 except Exception as error:
     print(error)
@@ -14,6 +21,29 @@ except Exception as error:
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
 @app.get("/")
-def root_get():
+def get_root():
     return {"data": "root page"}
+
+
+@app.get("/posts")
+def get_posts():
+    posts = cursor.execute("""SELECT * FROM posts""").fetchall()
+    # print(posts)
+    return {"data": posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def post_posts(post: Post):
+    post = cursor.execute(
+        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+        (post.title, post.content, post.published),
+    ).fetchone()
+    conn.commit()
+    return {"data": post}
